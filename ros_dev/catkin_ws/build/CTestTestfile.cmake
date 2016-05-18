0e4eca1042171cece22936e7d2cdc5ec70451a1c
Eric Hahn <erichahn@vt.edu>
2016-05-18 11:19:54 -0400

    moving to setting up packages

---
+++ ros_dev/catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/urk/Development/vtbolt_personal/ros_dev/catkin_ws/src
+# Build directory: /home/urk/Development/vtbolt_personal/ros_dev/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
---
+++ ros_dev/catkin_ws/build/gtest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/gtest
+# Build directory: /home/urk/Development/vtbolt_personal/ros_dev/catkin_ws/build/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
